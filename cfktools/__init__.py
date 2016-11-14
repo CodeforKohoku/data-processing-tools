f1d9ddbf642e098dcc2f7e08041e676ebf393cd6
osoken <osoken.devel@outlook.jp>
2016-11-14 19:34:10 +0900

    add csv2geojson function

--- cfktools/__init__.py
+++ cfktools/__init__.py
@@ -1,1 +1,36 @@
 # -*- coding: utf-8 -*-
+
+import csv
+import json
+
+
+def csv2geojson(input, encoding=None):
+    """CSV をインプットにして、GeoJSON として書き出し可能な ``dict`` 形式に変換する
+
+    :param input: インプットの CSV ファイル名
+    """
+    if isinstance(input, (str, bytes)):
+        with open(input, 'r', encoding=encoding or 'CP932') as fin:
+            return csv2geojson(fin)
+    reader = csv.DictReader(input)
+    fields = ("HID", "Type", "Kodomo", "Name", "Label", "AgeS", "AgeE",
+              "Full", "Open", "Close", "H24", "Memo", "Extra", "Temp",
+              "Holiday", "Night", "Add1", "Add2", "TEL", "FAX", "Owner",
+              "Ownership", "Proof", "Shanai", "Y", "X", "url", "Vacancy",
+              "VacancyDate")
+    data = {'type': 'FeatureCollection', "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    }}
+    a = []
+    for d in reader:
+        a.append({'properties': {
+            k: d[k] if k in d else None for k
+            in fields
+        }, "type": "Feature", "geometry": {
+            "type": "Point", "coordinates": [float(d['X']), float(d['Y'])]
+        }})
+    data['features'] = a
+    return data
