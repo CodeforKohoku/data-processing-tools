e5e276e0c1316c13612b2aae3358be1f697650f9
osoken <osoken.devel@outlook.jp>
2016-11-14 19:27:05 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='cfktools',
+    version='0.0.1',
+    author='osoken',
+    author_email='osoken.devel@outlook.jp',
+    url='https://github.com/CodeforKohoku/data-processing-tools',
+    description='data processing tools',
+    long_description='data processing tools',
+    license='MIT',
+    packages=['cfktools'],
+    install_requires=['fiona']
+)
