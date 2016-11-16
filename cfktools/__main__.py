bc7f6ded40b7cc3d717b1e4a3c21e8277d7960af
osoken <osoken.devel@outlook.jp>
2016-11-16 19:34:00 +0900

    add shp2geojson.py

--- cfktools/__main__.py
+++ cfktools/__main__.py
@@ -23,6 +23,12 @@ def csv2geojson_cli(args):
 
 def shp2Elementary_loc_cli(args):
     _input = args.input
+    if args.output == '-':
+        return json.dump(shp2Elementary_loc(_input), sys.stdout,
+                         ensure_ascii=False)
+    with open(args.output, 'w', encoding='utf-8') as _output:
+        return json.dump(shp2Elementary_loc(_input), _output,
+                         ensure_ascii=False)
 
 
 parser = ArgumentParser(prog='cfktools')
@@ -38,15 +44,16 @@ parser_csv2geojson.add_argument('-o', '--output', action='store',
                                 help='出力ファイル名 (default: 標準出力)')
 parser_csv2geojson.set_defaults(func=csv2geojson_cli)
 
-parser_shp2Elementary_loc = subparsers.add_parser('shp2Elementary_loc',
-                                           help='shp から geojson へ変換')
-parser_csv2geojson.add_argument('input', help='入力ファイル名か - (標準入力)')
-parser_csv2geojson.add_argument('-e', '--encoding', default=None,
-                                help='入力の文字コード（CP932）')
-parser_csv2geojson.add_argument('-o', '--output', action='store',
-                                dest='output', default='-',
-                                help='出力ファイル名 (default: 標準出力)')
-parser_csv2geojson.set_defaults(func=csv2geojson_cli)
+parser_shp2Elementary_loc = subparsers.add_parser(
+    'shp2Elementary_loc',
+    help='shp から geojson へ変換'
+)
+
+parser_shp2Elementary_loc.add_argument('input', help='入力ファイル名')
+parser_shp2Elementary_loc.add_argument('-o', '--output', action='store',
+                                       dest='output', default='-',
+                                       help='出力ファイル名 (default: 標準出力)')
+parser_shp2Elementary_loc.set_defaults(func=shp2Elementary_loc_cli)
 
 
 if __name__ == '__main__':
